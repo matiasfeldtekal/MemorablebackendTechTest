08ceb5b4573db03f213c8eb6cc8ad75ed1c1b453
Matias Feld <matias.feld@tekal.ai>
2022-06-14 15:48:48 -0300

    add second endpoint

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
 # Memorable Backend Tech Test
 
-Steps to run this project:
+## Steps to run this project:
 
 1. Run `npm install` command
-2. Setup database settings inside `data-source.ts` file
-3. Run `npm start` command
+2. Run `npm start` command
+
+
+## Endpoints examples
+- Create asset
+```graphql
+mutation {
+  createAsset {
+    id
+    name
+  }
+}
+```
+
+- Get all assets
+```graphql
+query {
+    getAllAssets{
+        id
+        name
+    }
+}
+```
--- src/assets/AssetResolver.ts
+++ src/assets/AssetResolver.ts
@@ -1,4 +1,4 @@
-import {Query, Resolver} from "type-graphql";
+import {Mutation, Query, Resolver} from "type-graphql";
 import {Service} from "typedi";
 import {Asset} from "./Asset";
 import {AppDataSource} from "../data-source";
@@ -16,5 +16,13 @@ export class AssetResolver {
                 id: MoreThan(0)
             }
         });
+    }
+
+    @Mutation((_returns) => Asset, {description: "Create an asset"})
+    async createAsset() {
+        // Example of how to use the database
+        const asset = new Asset();
+        asset.name = `New Asset ${Math.random()}`;
+        return await AppDataSource.getRepository(Asset).save(asset);
     }
 }
